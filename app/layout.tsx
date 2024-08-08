9e8ec16e6557cc9f8081c036cd460a0ccb4552ad
afifvdin <afifudin.fdn@gmail.com>
2024-08-08 19:24:29 +0700

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner"
-
-const inter = Inter({ subsets: ["latin"] })
+import { GeistSans } from "geist/font/sans"
 
 export const metadata: Metadata = {
   title: "Yer",
@@ -19,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full bg-background" suppressHydrationWarning>
-      <body className={cn(inter.className, "h-full bg-background")}>
+      <body className={cn(GeistSans.className, "h-full bg-background")}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Toaster />
           {children}
