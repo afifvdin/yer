97ef217d3936b37298e09102d69aafbaa23aaf73
afifvdin <afifudin.fdn@gmail.com>
2025-07-28 13:55:24 +0700

    opt out dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Home } from "./_components/home/home";
 import { Navbar } from "./_components/navbar/navbar";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "./components/ui/sonner";
 import { Uploader } from "./_components/uploader";
 import { useStore } from "./stores/use-store";
@@ -9,17 +8,15 @@ function App() {
   const sentencesLength = useStore((state) => state.getSentencesLength());
   return (
     <div className="bg-background h-full font-sans text-sm tracking-tight">
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Toaster />
-        {sentencesLength > 0 ? (
-          <>
-            <Navbar />
-            <Home />
-          </>
-        ) : (
-          <Uploader />
-        )}
-      </ThemeProvider>
+      <Toaster />
+      {sentencesLength > 0 ? (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ) : (
+        <Uploader />
+      )}
     </div>
   );
 }
